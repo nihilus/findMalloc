4fcd1def0e74f63d87a168beb3420bdcd932d40d
U-WIN-XP\Admin <nietzsche@lysator.liu.se>
2014-09-14 04:20:43 +0200

    Cleanup leftovers from VS2010.

--- findMalloc.cpp
+++ findMalloc.cpp
@@ -116,7 +116,7 @@ int does_exist(TFuncMallocWrapper new_func_malloc)
 int IDAP_init(void)
 {
 	if(inf.filetype != f_ELF && inf.filetype != f_PE)    {
-		error("Executable format must be PE or ELF, sorry.");
+		//error("Executable format must be PE or ELF, sorry.");
 		return PLUGIN_SKIP;
 	}
 
